a27d282b1c3a5c08c7682f973e311594f3ee437a
Gabriel <gdenmor2001@g.educaand.es>
2024-10-09 16:00:28 +0200

    Final commit

--- src/main/java/org/example/practica/Main.java
+++ src/main/java/org/example/practica/Main.java
@@ -1,6 +1,7 @@
 package org.example.practica;
 
 public class Main {
+    //Link de github: https://github.com/gdenmor/marshalling.git
     public static void main(String[] args) {
         Metodos.Main();
     }
